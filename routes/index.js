d5cc4efee580773d807ff27328a48b85e2e99634
Madhumitha-16 <madhu.br03@gmail.com>
2022-10-11 23:39:51 +0530

    DB connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost', // Replace with your host name
+  user: 'root',      // Replace with your database username
+  password: 'root',      // Replace with your database password
+  database: 'keep_app' // // Replace with your database Name
+}); 
+
+connection.connect(function (err) {
+    if (err) console.log("database connection failed",err);
+    else console.log("database connection successfull!!!!");
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
