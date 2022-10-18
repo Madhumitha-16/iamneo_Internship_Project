c5678c565eedcd6024a44675c0958d03b7e6653d
Madhumitha-16 <madhu.br03@gmail.com>
2022-10-18 18:50:40 +0530

    new css commit

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -15,7 +15,7 @@ function Note({ title, content, onDelete, id ,onEdit}) {
     
       <h2 style={{fontFamily: "sans-serif",fontSize:"28px"}}>{title}</h2>
       <p style={{color:"black"}}>{content}</p>
-      <p style={{fontSize:"10px"}}>created at:{showTime} - {date}</p>
+      <p style={{fontSize:"14px"}}>created at:{showTime} - {date}</p>
       <button onClick={() => onDelete(id,title)}>
         <MdDelete size={25} />
       </button>
